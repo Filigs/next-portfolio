8d9201970e5086e8f2ecbaf5bc2e68ec3e3a6c74
Filigs <40748182+Filigs@users.noreply.github.com>
2022-08-16 16:54:39 +0100

    sticky navbar, footer component from crypto-mockup

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import Socials from "./navbar/socials";
 
 export default function NavBar() {
   return (
-    <nav className="bg-white dark:bg-slate-900 shadow-sm dark:shadow-slate-500 transition-colors duration-300">
+    <nav className="bg-white dark:bg-slate-900 shadow-sm dark:shadow-slate-500 transition-colors duration-300 sticky lg:sticky min-w-full max-w-full inset-x-0 top-0 z-10">
       <div className="flex flex-row flex-wrap justify-between items-center p-4">
         <div className="text-slate-900 dark:text-white ">
           <span className="self-center text-xl font-semibold whitespace-nowrap">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ import("tailwindcss").Config;
 module.exports = {
   darkMode: "class",
   content: [
-    "./pages/**.js",
-    "./components/**.js",
-    "./node_modules/flowbite-react/**/*.js",
-    "./node_modules/flowbite/**/*.js",
+    "./pages/**.{js,ts,jsx,tsx}",
+    "./components/**.{js,ts,jsx,tsx}",
+    "./components/**/**.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {},
