110f04013ec2316c5e54c2ee3d9b71e092edc211
Filigs <40748182+Filigs@users.noreply.github.com>
2022-08-18 18:37:20 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
+// import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
-  usePageViews(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
+  // usePageViews(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
   return (
     <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
-      <GoogleAnalytics />
+      {/* <GoogleAnalytics /> */}
       <Component {...pageProps} />
     </ThemeProvider>
   );
