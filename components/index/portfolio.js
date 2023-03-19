1c27ee4604e224483ca7e959fc10b50735f9b535
Filigs <40748182+Filigs@users.noreply.github.com>
2023-03-19 22:36:08 +0000

    update contact form

--- components/index/portfolio.js
+++ components/index/portfolio.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { projects } from "../../lib/projects";
+import Link from "next/link";
 export default function Portfolio() {
   return (
     <div className="mt-30 bg-light dark:bg-transparent">
@@ -21,37 +22,38 @@ export default function Portfolio() {
         <div className="grid grid-cols-1 gap-8 mx-4 lg:grid-cols-2 xl:grid-cols-3">
           {projects.map((project) => {
             return (
-              <motion.div
-                key={project.id}
-                className="max-w-screen-xl shadow-lg rounded-xl bg-light dark:bg-darkMedium"
-                whileHover={{ scale: 1.05 }}
-              >
-                <div className="relative h-64 overflow-hidden sm:h-72 md:h-80 lg:h-96 rounded-t-xl">
-                  <Image
-                    alt={project.title + "'s image"}
-                    src={project.image}
-                    className="object-cover w-full h-full"
-                  />
-                </div>
-                <div className="flex flex-col justify-between px-6 py-8 card-content">
-                  <h3 className="mb-4 text-3xl font-bold tracking-tight text-primaryHover dark:text-primary">
-                    {project.title}
-                  </h3>
-                  <p className="mb-4 text-lg font-light text-darkDarker dark:text-white">
-                    {project.description.slice(0, 100) +
-                      (project.description.length > 100 ? "..." : "")}
-                  </p>
-
-                  <div className="flex flex-wrap items-center justify-between">
-                    <span className="inline-block px-3 py-1 mb-2 mr-2 text-base font-medium rounded-full bg-primaryHover text-lighter dark:bg-primary dark:text-light">
-                      {project.type}
-                    </span>
-                    <span className="text-sm text-darkLighter dark:text-lightest">
-                      {project.start}
-                    </span>
+              <Link href={project.url} key={project.id}>
+                <motion.div
+                  className="max-w-screen-xl shadow-lg rounded-xl bg-light dark:bg-darkMedium"
+                  whileHover={{ scale: 1.05 }}
+                >
+                  <div className="relative h-64 overflow-hidden sm:h-72 md:h-80 lg:h-96 rounded-t-xl">
+                    <Image
+                      alt={project.title + "'s image"}
+                      src={project.image}
+                      className="object-cover w-full h-full"
+                    />
                   </div>
-                </div>
-              </motion.div>
+                  <div className="flex flex-col justify-between px-6 py-8 card-content">
+                    <h3 className="mb-4 text-3xl font-bold tracking-tight text-primaryHover dark:text-primary">
+                      {project.title}
+                    </h3>
+                    <p className="mb-4 text-lg font-light text-darkDarker dark:text-white">
+                      {project.description.slice(0, 100) +
+                        (project.description.length > 100 ? "..." : "")}
+                    </p>
+
+                    <div className="flex flex-wrap items-center justify-between">
+                      <span className="inline-block px-3 py-1 mb-2 mr-2 text-base font-medium rounded-full bg-primaryHover text-lighter dark:bg-primary dark:text-light">
+                        {project.type}
+                      </span>
+                      <span className="text-sm text-darkLighter dark:text-lightest">
+                        {project.start}
+                      </span>
+                    </div>
+                  </div>
+                </motion.div>
+              </Link>
             );
           })}
         </div>
