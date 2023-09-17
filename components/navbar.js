1a8167bfe0d8fa8a1211b2ee1b60cc86dd3575b2
Filigs <40748182+Filigs@users.noreply.github.com>
2023-09-17 10:29:43 +0100

    update branding

--- components/navbar.js
+++ components/navbar.js
@@ -1,31 +1,38 @@
 import Image from "next/image";
 import branding from "/public/branding.png";
 import Link from "next/link";
-import { FiSend } from "react-icons/fi";
+import ThemeSwitch from "./theme";
+import { IoChatboxEllipses } from "react-icons/io5";
 
 export default function NavBar() {
   return (
     <nav>
-      <div className="flex flex-wrap items-center justify-between p-4 ">
-        <div className="inline-flex items-center gap-2 text-dark dark:text-light">
+      <div className="flex flex-wrap items-center justify-between lg:justify-around">
+        <div className="inline-flex items-center mx-4 text-dark dark:text-light">
           <Link href="/" passHref>
             <Image
               src={branding}
               alt={`Branding as a logo, representing the first letter of "Nuno" and "Martins"`}
-              width={80}
-              height={80}
-              className="scale-95 rounded-full"
+              width={128}
+              height={128}
+              className="scale-95 rounded-full lg:scale-125 drop-shadow-md"
             />
           </Link>
         </div>
-        <div className="inline-flex items-center border-0 text-dark dark:text-light">
-          <section className="p-1 mx-1 text-xl border-0 rounded-full cursor-pointer">
+        <div className="inline-flex items-center gap-4 mx-4 border-0 text-dark dark:text-light">
+          <section className="text-2xl border-0 rounded-full cursor-pointer lg:text-3xl ">
             <Link href="/contact-form" passHref>
-              <FiSend
+              <IoChatboxEllipses
                 alt="Contact me"
-                className="stroke-primary hover:stroke-primaryHover hover-transition"
+                className="fill-primary hover:fill-primaryHover hover-transition drop-shadow-md"
               />
             </Link>
+          </section>
+          <section
+            className="text-2xl border-0 rounded-full cursor-pointer lg:text-3xl drop-shadow-md"
+            alt="Switch color theme between dark and light mode"
+          >
+            <ThemeSwitch />
           </section>
         </div>
       </div>
